5be4aac6e67649e1c9c164695e60cbe536aeb6f7
arnaudon <alexis.arnaudon@epfl.ch>
2021-04-06 09:16:59 +0200

    update run script

--- examples/SIR/run_SIR_example.py
+++ examples/SIR/run_SIR_example.py
@@ -1,5 +1,4 @@
 import networkx as nx
-from pathlib import Path
 
 import numpy as np
 
@@ -8,19 +7,21 @@ from dyngdim.sir import analyse_graph
 
 if __name__ == "__main__":
 
-    times = np.logspace(-3.5, 1.2, 500)
-    betas = np.logspace(-0.7, 1.5, 10)
-    n_runs = 20
+    times = np.logspace(-3.5, 1.5, 1000)
+    betas = np.logspace(-1.2, 1.0, 50)
+    n_runs = 1000
     n_workers = 80
 
-    n_nodes = 500
+    n_nodes = 1000
+    k = 4
     ps = np.logspace(-2, -0.5, 10)
 
     for i, p in enumerate(ps):
         print(f"computing p = {p}")
 
-        folder = f"output_sm_{i}"
-        G = nx.newman_watts_strogatz_graph(n_nodes, 2, p, seed=0)
+        folder = f"output_{i}"
+
+        G = nx.newman_watts_strogatz_graph(n_nodes, k, p, seed=0)
         analyse_graph(
             G, times=times, betas=betas, n_runs=n_runs, n_workers=n_workers, folder=folder
         )
